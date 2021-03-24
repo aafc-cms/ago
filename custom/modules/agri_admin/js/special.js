5905fd05fc86cf0f86a29c0555540068acb99bab
olstjos <j@7pro.ca>
2021-03-24 01:25:39 -0400

    agrcms/d8#419 - Move special title and large mode to right below title, other fixes, recent functionality fixed for node add and seems to be ok for node edit.

--- custom/modules/agri_admin/js/special.js
+++ custom/modules/agri_admin/js/special.js
@@ -45,7 +45,6 @@ var Special = function() {
             CKEDITOR.instances["edit-body-0-value"].document.getById('sp_special_en').remove();
             var imgplaceholder = '<img src="https://wet-boew.github.io/themes-dist/GCWeb/img/520x200.png" alt="" class="pull-right img-responsive thumbnail">';
             if (Special.specialsettings.found_demo_topic) {
-		    console.log('1111');
               var uuid = Special.specialsettings.media_uuid;
               var entity_id = Special.specialsettings.media_entity_id;
               imgplaceholder = '<drupal-media data-entity-type="media" data-entity-uuid="'+uuid+'" data-view-mode="w0520px"></drupal-media>';
@@ -68,7 +67,6 @@ var Special = function() {
               var uuid = Special.specialsettings.media_uuid;
               var entity_id = Special.specialsettings.media_entity_id;
               imgplaceholder = '<drupal-media data-entity-type="media" data-entity-uuid="'+uuid+'" data-view-mode="w0520px"></drupal-media>';
-		    console.log('3333');
             }
             var oldcontentfr = CKEDITOR.instances["edit-body-etuf-fr-0-value"].getData();
             var newcontentfr = '<div id="sp_special_fr" class="row profile"><div class="col-md-6"><h1 property="name" id="wb-cont_fr">'+oldtitlefr+'</h1><p>1 ou 2 phrases d’introduction qui définissent les sous-sujets et les tâches principales qui peuvent être consultés sur cette page.</p><section class="followus"><h2>Suivez&nbsp;:</h2><ul><li><a href="#facebook" class="facebook wb-lbx wb-init wb-lbx-inited" id="wb-auto-4"><span class="wb-inv">Facebook</span></a></li><li><a href="#" class="twitter" rel="external"><span class="wb-inv">Twitter</span></a></li><li><a href="#youtube" class="youtube wb-lbx wb-init wb-lbx-inited" id="wb-auto-5"><span class="wb-inv">YouTube</span></a></li><li><a href="#" class="flickr" rel="external"><span class="wb-inv">Flickr</span></a></li><li><a href="#" class="pinterest" rel="external"><span class="wb-inv">Pinterest</span></a></li><li><a href="#" class="linkedin" rel="external"><span class="wb-inv">LinkedIn</span></a></li><li><a href="#" class="instagram" rel="external"><span class="wb-inv">Instagram</span></a></li><li><a href="#" class="rss" rel="external"><span class="wb-inv">Fil RSS</span></a></li><li><a href="#" class="email" rel="external"><span class="wb-inv">Abonnement par courriel</span></a></li><li><a href="#" class="periscope" rel="external"><span class="wb-inv">Periscope</span></a></li></ul></section><section id="facebook" class="mfp-hide modal-dialog modal-content overlay-def"><header class="modal-header"><h2 class="modal-title">Facebook</h2></header><div class="modal-body"><ul class="list-unstyled lst-spcd"><li><a href="#" rel="external">[Titre du premier compte Facebook]</a></li><li><a href="#" rel="external">[Titre du deuxième compte Facebook]</a></li></ul></div></section><section id="youtube" class="mfp-hide modal-dialog modal-content overlay-def"><header class="modal-header"><h2 class="modal-title">YouTube</h2></header><div class="modal-body"><ul class="list-unstyled lst-spcd"><li><a href="#" rel="external">[Titre du premier compte YouTube]</a></li><li><a href="#" rel="external">[Titre du deuxième compte YouTube]</a></li><li><a href="#" rel="external">[Titre du troisième compte YouTube]</a></li></ul></div></section></div><div class="col-md-6 mrgn-tp-sm hidden-sm hidden-xs">'+imgplaceholder+'</div></div>'
@@ -86,7 +84,6 @@ var Special = function() {
           var uuid = Special.specialsettings.media_uuid;
           var entity_id = Special.specialsettings.media_entity_id;
           imgplaceholder = '<drupal-media data-entity-type="media" data-entity-uuid="'+uuid+'" data-view-mode="w0520px"></drupal-media>';
-		    console.log('2222');
         }
         oldtitle = $('#edit-title-0-value').val();
         oldtitlefr = $('#edit-title-etuf-fr-0-value').val();
