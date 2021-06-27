ef97cd37da21faaf17ed57e12b1dc4b09f9bcfd9
olstjos <j@7pro.ca>
2021-06-27 11:40:02 -0400

    agrcms/d8#426 - Mobile fix for footer responsive class french/english.

---
+++ custom/scripts/user_delete.php
@@ -0,0 +1,15 @@
+<?php
+
+  $query = \Drupal::entityQuery('user')->condition('uid', 1, '>');
+  $uids = $query->execute();
+  $count_success = 0;
+  foreach ($uids as $vid => $uid) {
+    $user = \Drupal::entityTypeManager()->getStorage('user')->load($uid);
+    $user->delete();
+    \Drupal\agri_admin\AgriAdminHelper::addToLog('deleted:' . $uid, TRUE);
+    \Drupal\agri_admin\AgriAdminHelper::addMessage('deleted:'. $uid, TRUE);
+    if ($result !== FALSE) {
+      $count_success++;
+    }
+  }
+
