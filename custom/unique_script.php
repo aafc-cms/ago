99c05f1e211f1affa366ae15ba99cac5217c7ab5
olstjos <j@7pro.ca>
2023-08-03 19:18:51 +0000

    agrcms/d8#816 - Update the script to show just pdf, there's also some weird suspect corrupt record , adding a report for these.

--- custom/unique_script.php
+++ custom/unique_script.php
@@ -35,25 +35,83 @@ function getDuplicateFids() {
 }
 
 $fids = getDuplicateFids();
+$very_messed_up_files = [];
 
+$idx = 0;
 foreach ($fids as $fid) {
+  $file = \Drupal\file\Entity\File::load($fid);
+  if (!$file) {
+    unset($fids[$idx]);
+    continue;
+  }
+  $file_uri = str_replace('public://', '/sites/default/files/', $file->getFileUri());
+  if (stripos($file_uri, '.pdf') < 2 ) {
+    unset($fids[$idx]);
+    continue;
+  }
+  echo "fid=$fid";
+  echo "\n";
+
   $sha256 = getHashByFid($fid);
   $fids_with_matches = getFidsBySha256($sha256); 
   echo "---- sha256 = $sha256 ----";
   echo "\n";
-  foreach ($fids_with_matches as $fid_matched) {
-    echo "        fid = $fid_matched";
-    echo "\n";
-  }
+  $inner_idx = 0;
   foreach ($fids_with_matches as $fid_matched) {
     $file = \Drupal\file\Entity\File::load($fid_matched);
+    if (!$file) {
+      unset($fids_with_matches[$inner_idx]);
+      continue;
+    }
+    $file_uri = str_replace('public://', '/sites/default/files/', $file->getFileUri());
+    if (stripos($file_uri, '.pdf') < 2 ) {
+      $very_messed_up_files[] = [
+        'fid' => $fid_matched,
+        'file_uri' => $file_uri,
+        'filename' => $file->getFilename(),
+        'sha256' => $sha256,
+      ];
+      unset($fids_with_matches[$inner_idx]);
+      continue;
+    }
+    echo "        fid = /admin/content/files/replace/$fid_matched";
+    echo "\n";
+    $inner_idx++;
+  }
+  foreach ($fids_with_matches as $fid_matched) {
+    if (!$file) {
+      continue;
+    }
+    if (stripos($file_uri, '.pdf') < 2 ) {
+      unset($fids_with_matches[$inner_idx]);
+      continue;
+    }
     $file_uri = str_replace('public://', '/sites/default/files/', $file->getFileUri());
     echo "Is duplicate= " . $file_uri;
     echo "\n";
   }
+  $idx++;
+}
+
+echo "-----------------------------------------------------------------------------------";
+echo "\n";
+echo "------ report of corrupted filenames -----";
+foreach ($very_messed_up_files as $fid_matched) {
+  echo "-----------------------------------------------------------------------------------";
+  echo "\n";
+  echo "Filename matching file with problem       = " . $fid_matched['filename'];
+  echo "\n";
+  echo "fid matching corrupted fileuri duplicate  = /admin/content/files/replace/" . $fid_matched['fid'];
+  echo "\n";
+  echo "Has corrupted fileuri and is a duplicate  = " . $fid_matched['file_uri'];
+  echo "\n";
   echo "-----------------------------------------------------------------------------------";
   echo "\n";
 }
-
+echo "\n";
+echo "-----------------------------------------------------------------------------------";
+echo "\n";
 
 echo count($fids) . ' duplicates in total';
+
+
