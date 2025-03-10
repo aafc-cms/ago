2de32a75616e474015eeb36d3e8854b623d2af46
Stephen Mulvihill <smulvih2@gmail.com>
2025-03-10 07:44:52 -0700

    #894 - Remove aria-pressed from datatables pagination links

---
+++ custom/themes/aafctheme/js/aafc_datatables.js
@@ -0,0 +1,22 @@
+/**
+ * @file
+ * DataTable customizations for AAFC.
+ */
+
+(function (Drupal, once) {
+  Drupal.behaviors.removeAriaPressed = {
+    attach: function (context, settings) {
+      $(document).on("wb-ready.wb-tables wb-updated.wb-tables", function (event) {
+        var table = $(event.target);
+
+        // Ensure the pagination element exists
+        once('remove-aria-pressed', '.dataTables_paginate', context).forEach((pagination) => {
+          pagination.querySelectorAll('.paginate_button').forEach((button) => {
+            button.removeAttribute('aria-pressed');
+          });
+        });
+      });
+    }
+  };
+})(Drupal, once);
+
